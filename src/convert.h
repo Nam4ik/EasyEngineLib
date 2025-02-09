29796715fd5157a962fef04aaf43eaf35cff0bb6
Namilskyy <alive6863@gmail.com>
2025-02-09 18:35:21 +0300

    FIX: Moved header

--- src/convert.h
+++ src/convert.h
@@ -1,1 +1,23 @@
+#ifndef CONVERT_H
+#define CONVERT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cjson/cJSON.h>
+#include <time.h>
+
+time_t now = time(&now);
+struct tm *local = localtime(&now);
+
+char[] filename = "voicerec%d.log", local;
+
+system("touch", filename);
+
+#ifdef __WIN32  
+  return 0; 
+#else 
+  void transcribe_audio(const char *audio_file_path, bool use_json);
+  
+
+#endif 
 
