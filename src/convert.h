ea145a9f701ae30b259d459c14e5159bae0bb7a1
ArcaneDev <alive6863@gmail.com>
2025-02-09 21:40:21 +0300

    Update convert.h

--- src/convert.h
+++ src/convert.h
@@ -13,7 +13,10 @@ char[] filename = "voicerec%d.log", local;
 
 system("touch", filename);
 
+FILE fp* = filename
+
 #ifdef __WIN32  
+  fputs(fp, "Unsupported system type. Try use python voice recognizer");
   return 0; 
 #else 
   void transcribe_audio(const char *audio_file_path, bool use_json);
