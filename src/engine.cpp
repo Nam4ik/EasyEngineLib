2ac8aa2ffb59750a1c16a9b80429c332e85248d7
ArcaneDev <alive6863@gmail.com>
2025-02-10 21:50:48 +0300

    Update engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,10 +3,12 @@
 
 //TODO Python turtle UI and audio processsing with whisper.cpp 
 //TODO Fix .wav buffer and optimize project.
+//TODO Multythread optimisation
 //TODO Add OpenCV screen capture func (FOR DEBUG AND NEW FUNCS)
 //Need to remove useless funcs for optimise
 //Core dumped...
 //Maybe shared object will work?
+//Idk i compiled this but dont used and debuged :D 
 //... 
 
 //Developing for AthenaUI
@@ -25,10 +27,34 @@
   /_/  |_/_/    \___/ \__,_/ /_/ /_/\___//_____/ \___/_____/   | /_/|_| /
                                                                 \______/
 
+MIT License
+
+Copyright (c) 2025 ArcaneDev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+All rights reserved Arcane ® 2025.
 */
 
 
 //#include "lib/OpenCV.hpp"
+//#include "convert.h"
 #include "engine.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
